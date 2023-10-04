0cc092b5005d162fd92e532ae71a70393941f539
tereda92 <137965533+tereda92@users.noreply.github.com>
2023-10-04 05:55:56 -0700

    video 1

--- scripts/index.js
+++ scripts/index.js
@@ -28,17 +28,25 @@ const initialCards = [
 
 // Elements
 
-
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const profileEditNameInput = document.querySelector("#profile-edit-name");
+const profileTitleInput = document.querySelector('#profile-title-input');
+const profileDescriptionInput = document.querySelector("#profile-description-input");
 
 
-profileEditButton.addEventListener('click', () => {
-  profileEditModal.classList.add('modal__opened');
+console.log(profileName.textContent);
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
 });
 
-const profileEditCloseButton = document.querySelector("#profile__edit-close-button");
+const profileEditCloseButton = document.querySelector(
+  "#profile__edit-close-button"
+);
 
-profileEditCloseButton.addEventListener('click', () => {  
-  profileEditModal.classList.remove('modal__opened');
+profileEditCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
 });
